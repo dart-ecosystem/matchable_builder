516f6eded0cf2684450514a5411c32ba98bf4686
copydog <775150@qq.com>
2019-12-12 20:02:23 +0800

    refactor: String match accepts Pattern

--- matchable_builder/lib/src/filter/buildstep/filename_matcher.dart
+++ matchable_builder/lib/src/filter/buildstep/filename_matcher.dart
@@ -2,13 +2,21 @@ import 'package:build/src/builder/build_step.dart';
 import 'package:matchable_builder/src/filter/build_step_matcher.dart';
 
 class FilenameMatcher implements BuildStepMatcher {
-  RegExp fileNameRegExp;
+  Pattern filenamePattern;
 
-  FilenameMatcher(this.fileNameRegExp);
+  FilenameMatcher(this.filenamePattern);
 
   @override
   bool test(covariant BuildStep element) {
     final filename = element.inputId.uri.pathSegments.last;
-    return fileNameRegExp.hasMatch(filename);
+    if (filenamePattern is String) {
+      return filenamePattern == filename;
+    }
+
+    if (filenamePattern is RegExp) {
+      (filenamePattern as RegExp).hasMatch(filename);
+    }
+
+    return false;
   }
 }
--- matchable_builder/lib/src/filter/buildstep/package_matcher.dart
+++ matchable_builder/lib/src/filter/buildstep/package_matcher.dart
@@ -2,19 +2,21 @@ import 'package:build/src/builder/build_step.dart';
 import 'package:matchable_builder/matchable_builder.dart';
 
 class PackageMatcher implements BuildStepMatcher {
-  Pattern packageExpression;
+  Pattern package;
 
-  PackageMatcher(this.packageExpression);
+  PackageMatcher(this.package);
 
   @override
   bool test(BuildStep buildStep) {
     final packageName = buildStep.inputId.uri.scheme;
-    if (packageExpression is String) {
-      return packageExpression == packageName;
-    } else if (packageExpression is RegExp) {
-      (packageExpression as RegExp).hasMatch(packageName);
+    if (package is String) {
+      return package == packageName;
     }
 
-    throw Exception('packageExpression should be either String or RegExp');
+    if (package is RegExp) {
+      (package as RegExp).hasMatch(packageName);
+    }
+
+    return false;
   }
 }
